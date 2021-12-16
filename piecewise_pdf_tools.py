c2ea03a73004c4bf19cc98a0dd988cb8164ce3e5
Lewis Gross <ligross@wisc.edu>
2021-12-16 15:40:57 -0600

    changed if/elseif to print a message and return original vals if type is not constant or linear, added docstring

--- piecewise_pdf_tools.py
+++ piecewise_pdf_tools.py
@@ -1,20 +1,29 @@
 import numpy
-import os,sys
+import os
 
-# TO DO make these formulas right for constant or 
 def createNormalizedPDF(E_grid,bin_vals,N,type):
+    '''
+    Accepts an energy grid with N points and bin values evaluated on the lower boundary of
+    each energy group, except the last bin_vals entry is the energy evaluated at the highest 
+    energy. Checks to see if the distribution is normalized. If not, compute and returns
+     bin_vals for a normalized pdf. Works for piecewise constant and piecewise linear distributions
+    '''
     area = 0.0
     if(type=='constant'):
         # assumes pdf is piecewise with constant value in each region
         for i in range(0,N-1):
             print(i)
             area += bin_vals[i]*(E_grid[i+1] - E_grid[i])
-    else:
+    elif(type=='linear'):
         # assumes pdf is piecewise linear in each region
         for i in range(0,N-1):
             print(i)
             area += (bin_vals[i]+bin_vals[i+1])*(E_grid[i+1] - E_grid[i])/2
-    # do stuff to normalize
-    nomralized_bin_vals = bin_vals/area
-    print(nomralized_bin_vals)
+    else:
+        print("Type not recognized, enter linear or constant. Original bin_vals returned")
+        return bin_vals
+
+    # divide bin_vals by normalization factor so that pdf is normalized to one
+    print(area)
+    normalized_bin_vals = bin_vals/area
     return normalized_bin_vals

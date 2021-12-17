86874c94036ea46fe2d143e65078ff01de121d34
Lewis Gross <ligross@wisc.edu>
2021-12-16 18:00:30 -0600

    cleaned up printing

--- run_examples.py
+++ run_examples.py
@@ -7,13 +7,17 @@ import matplotlib
 # energy grid given in order from lowest energy to highest energy
 E_grid = np.array([0.0,0.25,0.5,0.75,1.0])
 # value at each energy grid point
-bin_vals = np.array([1,5,3,2,2])
+bin_vals = np.array([1,5,4,2,2])
 # number of energy grid points 
 # TODO this may be unnecessary, 
 # may be better practice to compute this N in creatNormalizedPDF
 N = 5
 constant_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"constant")
-print("original bin vals:",bin_vals,'\n',"returned bin vals",constant_normalized_bin_vals)
+print("original bin vals:",bin_vals, "returned bin vals:",constant_normalized_bin_vals)
+print('\n')
+constant_normalized_bin_vals_check = pdft.createNormalizedPDF(E_grid,constant_normalized_bin_vals ,N,"constant")
+print("original bin vals:",bin_vals,"returned bin vals:",constant_normalized_bin_vals)
+print('\n')
 
 # piecewise linear
 # energy grid given in order from lowest energy to highest energy
@@ -25,11 +29,15 @@ bin_vals = np.array([1,5,3,2,6])
 # may be better practice to compute this N in creatNormalizedPDF
 N = 5
 linear_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"linear")
-print("original bin vals:",bin_vals,'\n',"returned bin vals",linear_normalized_bin_vals)
+print("original bin vals:",bin_vals,"returned bin vals:",linear_normalized_bin_vals)
+print('\n')
+linear_normalized_bin_vals_check = pdft.createNormalizedPDF(E_grid,linear_normalized_bin_vals,N,"linear")
+print("original bin vals:",bin_vals,"returned bin vals:",linear_normalized_bin_vals)
+print('\n')
 
 # test what happens when type passed is not constant or linear
 failed_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"YOLO")
-print("original bin vals:",bin_vals,'\n',"returned bin vals",failed_normalized_bin_vals)
+print("original bin vals:",bin_vals,"returned bin vals:",failed_normalized_bin_vals)
 
 
 # SET UP EXAMPLE CDFS

cb9cf19710b5b22c79e74782f4d1441d9c01ba6a
Lewis Gross <ligross@wisc.edu>
2021-12-16 22:23:12 -0600

    some formatting, needs testing

--- piecewise_pdf_tools.py
+++ piecewise_pdf_tools.py
@@ -1,56 +1,52 @@
 import numpy as np
 import os
 
-def createNormalizedPDF(E_grid,bin_vals,N,type):
+def createNormalizedPDF(E_GRID, BIN_VALS , N, TYPE):
     '''
     Accepts an energy grid with N points and bin values evaluated on the lower boundary of
-    each energy group, except the last bin_vals entry is the energy evaluated at the highest 
+    each energy group, except the last BIN_VALS entry is the energy evaluated at the highest problem
     energy. Checks to see if the distribution is normalized. If not, compute and returns
-     bin_vals for a normalized pdf. Works for piecewise constant and piecewise linear distributions
+    BIN_VALS for a normalized pdf. Works for piecewise constant and piecewise linear distributions
     '''
     area = 0.0
-    if(np.size(E_grid)!=N):
-        print("The energy grid expected ", N, " points, but the grid size is: ",np.size(E_grid),". Original bin_vals returned")
-        return bin_vals
-    if(type=='constant'):
+    if np.size(E_GRID) != N:
+        print("The energy grid expected ", N, " points, but the grid size is: ", np.size(E_GRID), ". Original BIN_VALS returned")
+        return BIN_VALS
+    if TYPE == 'constant':
         # assumes pdf is piecewise with constant value in each region
-        for i in range(0,N-1):
-            area += bin_vals[i]*(E_grid[i+1] - E_grid[i])
-    elif(type=='linear'):
+        for i in range(0, N-1):
+            area += BIN_VALS[i]*(E_GRID[i+1] - E_GRID[i])
+    elif TYPE == 'linear':
         # assumes pdf is piecewise linear in each region
-        for i in range(0,N-1):
-            area += (bin_vals[i]+bin_vals[i+1])*(E_grid[i+1] - E_grid[i])/2
+        for i in range(0, N-1):
+            area += (BIN_VALS[i]+BIN_VALS[i+1])*(E_GRID[i+1] - E_GRID[i])/2
     else:
-        print("Type: ",type," not recognized, enter linear or constant. Original bin_vals returned")
-        return bin_vals
+        print("TYPE: ", TYPE, " unrecognized, enter linear or constant. Original BIN_VALS returned")
+        return BIN_VALS
 
-    # divide bin_vals by normalization factor so that pdf is normalized to one
-    print("original area under provided data:",area)
-    normalized_bin_vals = bin_vals/area
-    return normalized_bin_vals
+    # divide BIN_VALS by normalization factor so that pdf is normalized to one
+    print("original area under provided data:", area)
+    normalized_bin_values = BIN_VALS/area
+    return normalized_bin_values
 
 
-# def computeCumulativeDensities(E_grid,bin_vals,N,type):
-#     normalized_bin_vals = createNormalizedPDF(E_grid,bin_vals,N,type)
-#     cumulative_density = 0.0
-#     cum_vals = np.zeros(N,float)
-#     if(np.size(E_grid)!=N):
-#         print("The energy grid expected ", N, " points, but the grid size is: ",np.size(E_grid),". Original bin_vals returned")
-#         return 
-#     if(type=='constant'):
-#         # assumes pdf is piecewise with constant value in each region
-#         for i in range(0,N):
-#             cum_vals[i+1] = cum_vals[i] + (E_grid[i+1] - E_grid[i])*bin_vals[i]
-#     elif(type=='linear'):
-#         # assumes pdf is piecewise linear in each region
-#         for i in range(0,N-1):
-#             cum_vals[i+1] += (bin_vals[i]+bin_vals[i+1])*(E_grid[i+1] - E_grid[i])/2
-#     else:
-#         print("Type: ",type," not recognized, enter linear or constant. Original bin_vals returned")
-#         return bin_vals #TODO is this appropriate?
+def computeCumulativeDensities(E_GRID, BIN_VALUES, N, TYPE):
+    normalized_bin_values = createNormalizedPDF(E_GRID, BIN_VALUES, N, TYPE)
+    cumulative_values = np.zeros(N, float)
+    if np.size(E_GRID) != N:
+        print("The energy grid expected ", N, " points, but the grid size is: ", np.size(E_GRID), ". Original BIN_VALS returned")
+        return cumulative_values
+    if TYPE == 'constant':
+        # assumes pdf is piecewise with constant value in each region
+        for i in range(0, N):
+            cumulative_values[i+1] = cumulative_values[i] + (E_GRID[i+1] - E_GRID[i])*normalized_bin_values[i]
+    elif TYPE == 'linear':
+        # assumes pdf is piecewise linear in each region
+        for i in range(0, N-1):
+            cumulative_values[i+1] += (normalized_bin_values[i]+normalized_bin_values[i+1])*(E_GRID[i+1] - E_GRID[i])/2
+    else:
+        print("TYPE: ", TYPE, " not recognized, enter linear or constant. normalized_bin_values returned")
+        return normalized_bin_values #TODO is this appropriate?
 
-#     # TODO what is the best way to check for normalization, is it exception handling?
-#     if(np.isclose(1.0,cum_vals[-1],rtol=0.000001)):
-#         normalized = True
-#         return cum_vals
-#     return cum_vals
+    # TODO what is the best way to check for normalization and what to return
+    return cumulative_values if np.isclose(1.0, cumulative_values[-1], rtol=0.0000001) else cumulative_values

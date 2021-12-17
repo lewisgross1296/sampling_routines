f0ead85bcc22c2d7e961cb48710bc89a0ea90719
Lewis Gross <ligross@wisc.edu>
2021-12-16 22:00:00 -0600

    better formatting (turned on linter), removed some print statements

--- run_examples.py
+++ run_examples.py
@@ -5,39 +5,33 @@ import matplotlib
 # SET UP EXAMPLE PDFS
 # piecewise constant
 # energy grid given in order from lowest energy to highest energy
-E_grid = np.array([0.0,0.25,0.5,0.75,1.0])
+E_GRID = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
 # value at each energy grid point
-bin_vals = np.array([1,5,4,2,2])
-# number of energy grid points 
-# TODO this may be unnecessary, 
-# may be better practice to compute this N in creatNormalizedPDF
+BIN_VALS = np.array([1, 5, 4, 2, 2])
+# number of energy grid points
 N = 5
-constant_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"constant")
-print("original bin vals:",bin_vals, "returned bin vals:",constant_normalized_bin_vals)
+CONSTANT_NORMALIZED_BIN_VALS = pdft.createNormalizedPDF(E_GRID, BIN_VALS, N, "constant")
+print("original bin vals:", BIN_VALS, "returned bin vals:", CONSTANT_NORMALIZED_BIN_VALS)
 print('\n')
-constant_normalized_bin_vals_check = pdft.createNormalizedPDF(E_grid,constant_normalized_bin_vals ,N,"constant")
-print("original bin vals:",bin_vals,"returned bin vals:",constant_normalized_bin_vals)
+CONSTANT_NORMALIZED_BIN_VALS_CHECK = pdft.createNormalizedPDF(E_GRID, CONSTANT_NORMALIZED_BIN_VALS , N, "constant")
 print('\n')
 
 # piecewise linear
 # energy grid given in order from lowest energy to highest energy
-E_grid = np.array([0.0,0.25,0.5,0.75,1.0])
+E_GRID = np.array([0.0, 0.25, 0.5, 0.75, 1.0])
 # value at each energy grid point
-bin_vals = np.array([1,5,3,2,6])
-# number of energy grid points 
-# TODO this may be unnecessary, 
-# may be better practice to compute this N in creatNormalizedPDF
+BIN_VALS = np.array([1, 5, 3, 2, 6])
+# number of energy grid points
 N = 5
-linear_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"linear")
-print("original bin vals:",bin_vals,"returned bin vals:",linear_normalized_bin_vals)
-print('\n')
-linear_normalized_bin_vals_check = pdft.createNormalizedPDF(E_grid,linear_normalized_bin_vals,N,"linear")
-print("original bin vals:",bin_vals,"returned bin vals:",linear_normalized_bin_vals)
-print('\n')
+LINEAR_NORMALIZED_BIN_VALS = pdft.createNormalizedPDF(E_GRID, BIN_VALS, N, "linear")
+print("original bin vals:", BIN_VALS, "returned bin vals:", LINEAR_NORMALIZED_BIN_VALS)
+print("\n")
+LINEAR_NORMALIZED_BIN_VALS_CHECK = pdft.createNormalizedPDF(E_GRID, LINEAR_NORMALIZED_BIN_VALS, N, "linear")
+print("\n")
 
 # test what happens when type passed is not constant or linear
-failed_normalized_bin_vals = pdft.createNormalizedPDF(E_grid,bin_vals,N,"YOLO")
-print("original bin vals:",bin_vals,"returned bin vals:",failed_normalized_bin_vals)
+FAILED_NORMALIZED_BIN_VALS = pdft.createNormalizedPDF(E_GRID, BIN_VALS, N, "YOLO")
+print("original bin vals:", BIN_VALS, "returned bin vals:", FAILED_NORMALIZED_BIN_VALS)
 
 
 # SET UP EXAMPLE CDFS
